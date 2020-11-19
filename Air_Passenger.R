5ab68b6b434fc73a0b321f7ef116b4e27e3af40a
vibhutivadje <vibhutivadje@gmail.com>
2020-11-19 00:39:11 -0800

    dataframe

--- Air_Passenger.R
+++ Air_Passenger.R
@@ -5,3 +5,9 @@
 install.packages("forecast")
 library(forecast)
 library(zoo)
+
+## CREATE DATA FRAME. 
+#Grocery.data <- read.csv("673_case1.csv")
+Air_Passenger.data <- read.csv(file.choose(), header = TRUE)
+# See the first 6 records of the file.
+head(Air_Passenger.data)
