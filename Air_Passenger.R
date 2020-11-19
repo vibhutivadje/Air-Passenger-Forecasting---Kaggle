c9fca3551e2e8245805ec306d984dd62342a8d32
vibhutivadje <vibhutivadje@gmail.com>
2020-11-19 00:48:17 -0800

    Plots

--- Air_Passenger.R
+++ Air_Passenger.R
@@ -11,3 +11,21 @@ library(zoo)
 Air_Passenger.data <- read.csv(file.choose(), header = TRUE)
 # See the first 6 records of the file.
 head(Air_Passenger.data)
+
+## USE ts() FUNCTION TO CREATE TIME SERIES DATA SET.
+Air_Passenger.ts <- ts(Air_Passenger.data$Passengers, 
+               start = c(1949, 1), end = c(1960, 12), freq = 12)
+
+Air_Passenger.ts
+
+## Use plot() to plot time series data  
+plot(Air_Passenger.ts, 
+     xlab = "Time", ylab = "Passengers (in Hundreds) ", 
+     ylim = c(100, 700), main = "Monthly Air Passenger", col = "blue")
+
+# Use stl() function to plot times series components of the original data. 
+# The plot includes original data, trend, seasonal, and reminder 
+# (level and noise component).
+Air_Passenger.stl <- stl(Air_Passenger.ts, s.window = "periodic")
+Air_Passenger.stl
+autoplot(Air_Passenger.stl, main = "Gorcery Store Time Series Components")
