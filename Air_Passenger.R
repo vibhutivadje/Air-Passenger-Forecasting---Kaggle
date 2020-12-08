0fbfdddbeae8106310c01f38f6411d4e70464c0f
vibhutivadje <vibhutivadje@gmail.com>
2020-12-07 22:25:34 -0800

    BoxPlot

--- Air_Passenger.R
+++ Air_Passenger.R
@@ -52,3 +52,7 @@ AirPassenger.stl <- stl(AirPassenger.ts, s.window = "periodic")
 
 autoplot(AirPassenger.stl, main = "Airpassenger Time Series Components")
 
+## 4-d: Box plot
+boxplot(AirPassenger.ts~cycle(AirPassenger.ts), xlab="Passenger Numbers ('000)", ylab="Months", col=rgb(0.1,0.9,0.3,0.4), 
+        main="Monthly Air Passengers Boxplot from 1950 to 1960", notch=FALSE)
+
