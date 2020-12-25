7993660b32c23a27dd4b4133e1d3a383e933b333
vibhutivadje <vibhutivadje@gmail.com>
2020-12-25 14:50:25 -0800

    Apply Forecasting Method

--- Air_Passenger.R
+++ Air_Passenger.R
@@ -99,4 +99,38 @@ arrows(1959.1, 670, 1960.8, 670, code = 3, length = 0.1,
        lwd = 1, angle = 30)
 arrows(1961.1, 670, 1962.5, 670, code = 3, length = 0.1,
        lwd = 1, angle = 30)
+## 6. APPLY FORECASTING METHODS
+
+## 6-a: REGRESSION BASED MODELS 
+
+## Apply Regression Models with trend and seasonality and apply forecast() for validataion period.
+
+## i.  Regression model with linear trend and seasonality
+
+train.lin.season <- tslm(train.ts ~ trend + season)
+summary(train.lin.season)
+train.lin.season.pred <- forecast(train.lin.season, h = nValid, level = 0)
+
+# Plot predictions for linear trend and seasonality forecast.
+plot(train.lin.season.pred$mean, 
+     xlab = "Time", ylab = "Air Passengers", ylim = c(50, 720), bty = "l",
+     xlim = c(1949, 1962.25), main = "Linear Trend and Seasonality Forecast", 
+     col = "blue", lwd =2) 
+lines(train.lin.season.pred$fitted, col = "blue", lwd = 2)
+lines(train.ts, col = "black", lty = 1)
+lines(valid.ts, col = "black", lty = 1)
+
+# Plot on chart vertical lines and horizontal arrows describing
+# training, validation, and future prediction intervals.
+lines(c(1958.9, 1958.9), c(0, 720))
+lines(c(1960.95, 1960.9), c(0, 720))
+text(1954, 720, "Training", cex = 0.75)
+text(1959.9, 720, "Validation", cex = 0.75)
+#text(1961.8, 720, "Future", cex = 0.75)
+arrows(1958.7, 670, 1948.7, 670, code = 3, length = 0.1,
+       lwd = 1, angle = 30)
+arrows(1959.1, 670, 1960.8, 670, code = 3, length = 0.1,
+       lwd = 1, angle = 30)
+#arrows(1961.1, 670, 1962.5, 670, code = 3, length = 0.1,
+#      lwd = 1, angle = 30)
 
